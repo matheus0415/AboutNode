40cee64e405a46ffedb342f6efa3a712ba31da06
Matheus Santos <somathsant@gmail.com>
2022-06-20 23:51:13 -0300

    First Commit

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,11 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+})
+
+//DESAFIO
+// Mulher chinesa com menor salario
+// funçao map, reduce
